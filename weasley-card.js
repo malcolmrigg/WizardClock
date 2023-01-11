481066e331262d927f5ebcacab2425111b59d849
arniebarni <>
2023-01-11 19:39:08 +0100

    handle undefined friendly_name

--- weasley-card.js
+++ weasley-card.js
@@ -252,7 +252,8 @@ class WeasleyClockCard extends HTMLElement {
           )
           :  this.lostState;
 	
-        if (this.exclude.includes(this._hass.states["zone." + stateStr].attributes.friendly_name)) {
+        if (this.exclude.includes(stateStr) ||
+      	  this._hass.states["zone." + stateStr] && this._hass.states["zone." + stateStr].attributes && this._hass.states["zone." + stateStr].attributes.friendly_name) {
           stateStr = this.lostState;
 	}
         const stateVelo = state && state.attributes ? (
